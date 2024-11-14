da5fed442859c496c52389e5b8dd67a07cf70d3d
kevalbra <kev.albrandt.24@heilbronn.dhbw.de>
2024-11-14 10:49:35 +0100

    startproject

---
+++ src/main/java/com/kevalbra/sonatesecure/SonateSecure.java
@@ -0,0 +1,14 @@
+package com.kevalbra.sonatesecure;
+
+import com.cthiebaud.passwordvalidator.*;
+
+
+public class SonateSecure implements PasswordValidator{
+
+    @Override
+    public ValidationResult validate(String arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validate'");
+    }
+    
+}
